ecd7ea1e5f48b34e237a8a487901d6819e4e1c3c
Kyle Adkins <kyle.m.adkins@gmail.com>
2024-10-03 22:06:21 -0400

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://kylemadkins.com",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: 'server',
+  output: "static",
   adapter: netlify(),
 });
