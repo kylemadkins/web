9cb4eaa2bce01890f413ce601a7c5c3240911681
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-01-09 17:52:46 -0500

    Design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,21 @@
-import { SemicolonPreference } from 'typescript'
+import { SemicolonPreference } from "typescript";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				"archivo-narrow": '"Archivo Narrow", sans-serif',
-				"oswald": '"Oswald", sans-serif',
-			},
-			colors: {
-				"black": "rgb(24, 24, 27)",
-			},
-			boxShadow: {
-				"simple": "0px 5px 0px 0px rgba(0, 0, 0, 0.15)",
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "archivo-narrow": '"Archivo Narrow", sans-serif',
+        oswald: '"Bricolage Grotesque", sans-serif',
+      },
+      colors: {
+        black: "rgb(24, 24, 27)",
+      },
+      boxShadow: {
+        simple: "0px 5px 0px 0px rgba(0, 0, 0, 0.15)",
+      },
+    },
+  },
+  plugins: [],
+};
