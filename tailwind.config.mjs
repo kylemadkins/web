56aaf64526cfcf7b9252feda5f7291042d43b81c
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-01-10 17:53:31 -0500

    Changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,7 +12,10 @@ export default {
       colors: {
         black: "rgb(24, 24, 27)",
         yellow: "#f4bd1a",
-        gold: "#8c7329"
+        gold: "#8c7329",
+        green: "#47663B",
+        "dark-green": "#1F4529",
+        "light-green": "#E8ECD7"
       },
       boxShadow: {
         simple: "0px 5px 0px 0px rgba(0, 0, 0, 0.15)",
